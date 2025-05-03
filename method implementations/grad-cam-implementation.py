e197000d3447960ca28237211875566e6198f682
mnek1t <mednikita2004@gmail.com>
2025-05-03 23:28:08 +0300

    Grad Cam refactoring v2

--- method implementations/grad-cam-implementation.py
+++ method implementations/grad-cam-implementation.py
@@ -1,15 +1,16 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.keras.backend as K
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt
 import cv2
 import quantus
 import time
-start_exececution_time = time.perf_counter()
+
+start_execution_time = time.perf_counter()
+
 def generate_gradcam_heatmap(model, img_tensor, class_index, conv_layer_name="mixed10"):
-    grad_model = tf.keras.models.Model(
+    grad_model = Model(
         [model.inputs],
         [model.get_layer(conv_layer_name).output, model.output]
     )
@@ -30,10 +31,11 @@ def generate_gradcam_heatmap(model, img_tensor, class_index, conv_layer_name="mi
 
     return heatmap
 
-def make_explain_func(cached_heatmap):
-    def explain_func(model, inputs, targets):
-        return np.expand_dims(cached_heatmap, axis=0)
-    return explain_func
+def explain_func(model, inputs, targets):
+    heatmap = generate_gradcam_heatmap(model, inputs, targets[0]) 
+    heatmap_resized = cv2.resize(heatmap, (224, 224))
+    a_batch = np.expand_dims(heatmap_resized, axis=0)
+    return a_batch
 
 def plot_results(title, input, img):
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
@@ -49,13 +51,13 @@ def plot_results(title, input, img):
 
 CLASS_NAMES = ['glass', 'cardboard', 'metal', 'paper', 'plastic', 'trash']
 MODEL_PATH = "D:\\University\\Bachelor Thesis\\xai methods\\results\\best_inception_model.keras"
-IMAGE_PATH = "D:\\University\\Bachelor Thesis\\xai methods\\coca_cola_trash.jpeg"
+IMAGE_PATH = "D:\\University\\Bachelor Thesis\\xai methods\\bcaa-trash.jpg"
+
 INTENSITY = 0.5
 
 model = load_model(MODEL_PATH)
-
 img = image.load_img(IMAGE_PATH, target_size=(224, 224))
-pixels = np.asarray(img).astype('float32')
+pixels = np.asarray(img).astype('float32') / 255.0 
 x_batch = np.expand_dims(pixels, axis=0)
 
 s_batch = np.zeros((1, 224, 224, 3), dtype=np.float32)
@@ -69,7 +71,6 @@ pred_class = np.argmax(preds[0])
 y_batch = np.array([pred_class])
 
 heatmap = generate_gradcam_heatmap(model, x_batch, pred_class)
-print('heatmap', heatmap.shape)
 heatmap_resized = cv2.resize(heatmap, (224, 224))
 a_batch = np.expand_dims(heatmap_resized, axis=0)
 
@@ -80,8 +81,6 @@ overlay = np.clip(overlay / 255.0, 0, 1)
 
 plot_results("Grad-CAM Overlay", overlay, img)
 
-explain_func = make_explain_func(heatmap_resized)
-
 metrics = {
     "Robustness": quantus.AvgSensitivity(
         nr_samples=2,
@@ -91,54 +90,51 @@ metrics = {
         perturb_func=quantus.perturb_func.uniform_noise,
         similarity_func=quantus.similarity_func.difference,
         abs=True,
-        normalise=False,
+        normalise=True,
         aggregate_func=np.mean,
         return_aggregate=True,
-        disable_warnings=True,
         display_progressbar=True
     ),
     "Faithfulness": quantus.FaithfulnessCorrelation(
-        nr_runs=10,
-        subset_size=3,
+        nr_runs=15,
+        subset_size=20,
         perturb_baseline="black",
         perturb_func=quantus.baseline_replacement_by_indices,
         similarity_func=quantus.similarity_func.correlation_pearson,
         return_aggregate=True,
-        disable_warnings=True,
     ),
-    "Complexity": quantus.Sparseness(
-        return_aggregate=True,
-        disable_warnings=True,
+   "Complexity":  quantus.Sparseness(
+        normalise=True,
+        aggregate_func=np.mean,
+        return_aggregate=True
     ),
-    "Effective Complexity": quantus.EffectiveComplexity(
+    "Effective Complexity":  quantus.EffectiveComplexity(
+        normalise=True,
+        aggregate_func=np.mean,
         return_aggregate=True,
-        disable_warnings=True,
     ),
     "Localisation": quantus.RelevanceRankAccuracy(
         abs=True,
-        normalise=False,
+        normalise=True,
         aggregate_func=np.mean,
         return_aggregate=True,
-        disable_warnings=True,
     ),
     "Selectivity": quantus.Selectivity(
         perturb_baseline="black",
         patch_size=16,
         perturb_func=quantus.baseline_replacement_by_indices,
         abs=True,
-        normalise=False,
+        normalise=True,
         return_aggregate=True,
-        disable_warnings=True,
         display_progressbar=True
     ),
     "SensitivityN": quantus.SensitivityN(
         features_in_step=256,
         n_max_percentage=0.3,
-        similarity_func=quantus.similarity_func.cosine,
+        similarity_func=lambda a, b, **kwargs: quantus.similarity_func.abs_difference(np.array(a), np.array(b)),
         perturb_baseline="black",
         perturb_func=quantus.baseline_replacement_by_indices,
         return_aggregate=True,
-        disable_warnings=True,
         display_progressbar=True
     )
 }
@@ -168,4 +164,4 @@ for name, score in results.items():
     print(f"{name}: {score}")
 
 end_execution_time = time.perf_counter()
-print(f"\nGrad-CAM took {end_time - start_time:.2f} seconds.")
+print(f"\nGrad-CAM took {end_execution_time - start_execution_time:.2f} seconds.")
